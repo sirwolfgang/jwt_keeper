83536e13dd8fa86a318a78364cb397b1e5af7a84
David Rivera <david.r.rivera193@gmail.com>
2015-12-29 16:19:19 -0800

    comments

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -9,6 +9,8 @@ require 'hotel/version'
 # This module encapsulates the functionality
 # for generating, retrieving, and validating an
 # auth token
+# @attr [Hotel::Configuration] the configuration
+# @attr [Hote::Token] a token instance
 module Hotel
 
   attr_accessor :configuration, :token
