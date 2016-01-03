c14e8245f3b0499529e3c1966400d631d3594e07
Zane Wolfgang Pickett <Zane.Wolfgang.Pickett@Gmail.com>
2016-01-02 20:00:54 -0800

    Fix issues with install template being out of date

--- lib/generators/templates/keeper.rb
+++ lib/generators/templates/keeper.rb
@@ -13,13 +13,16 @@ Keeper.configure do |config|
   #   ES256 - ECDSA using P-256 and SHA-256
   #   ES384 - ECDSA using P-384 and SHA-384
   #   ES512 - ECDSA using P-521 and SHA-512
-  # config.hashing_method   = 'HS512',
+  # config.algorithm        = 'HS512'
+
+  # the secret in which you data is hash with
+  # config.secret           = 'secret'
 
   # the issuer of the tokens
-  # config.issuer           = 'api.example.com',
+  # config.issuer           = 'api.example.com'
 
   # the default audience of the tokens
-  # config.default_audience = 'example.com',
+  # config.audience         = 'example.com'
 
   # the location of redis config file
   # config.redis_connection = Redis.new(connection_options)
