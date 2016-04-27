a92cb2091e5169dfcc7d036268d674364b3083fc
Zane Wolfgang Pickett <sirwolfgang@users.noreply.github.com>
2016-04-26 22:23:29 -0700

    Fix issue with generator naming

--- lib/generators/jwt_keeper/install/install_generator.rb
+++ lib/generators/jwt_keeper/install/install_generator.rb
@@ -1,6 +1,6 @@
 require 'rails/generators/base'
 
-module JWTKeeper
+module JwtKeeper
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../../../templates', __FILE__)
 
